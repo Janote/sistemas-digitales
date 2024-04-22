4e2ddeb19035a6278d104ba5818ee09024573353
Janote <janoiangonzalez@gmail.com>
2024-04-22 16:59:03 -0300

    hice hasta el checkpoint 2

---
+++ verificador.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+logisim-evolution $1 -tty table > resultados.txt
+echo "Resultados guardados en resultados.txt"
+echo "Se encontro que `diff -qs expected.txt resultados.txt`."
+   ( echo "A   \tB   \tOP\tN\tZ\tV\tC\tRES" > diff.txt & diff expected.txt resultados.txt --color >> diff.txt;
+   if [ $? -ne 0 ]
+   then
+	   echo "Revisa el archivo diff.txt para ver las diferencias con el resultado esperado."
+   else
+      echo "El Taller parece funcionar!"
+   fi
+   )
